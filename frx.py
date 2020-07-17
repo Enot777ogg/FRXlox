9633cd3e9ebb7657f26845fee49db1ad1cdb05a8
Enot777ogg <63520770+Enot777ogg@users.noreply.github.com>
2020-07-18 02:56:40 +0300

    Update frx.py

--- frx.py
+++ frx.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
+!/usr/bin/python
 # My first program on Python!
-import time (1)
+import os 
+import time
 print ("DDOS ATAKA!!!") 
 print ("До самоуничтожения 10 фарухов из 10") 
-import time (1)
 print ("9 фаррухов из 10") 
 time.sleep (1)
 print ("8 фаррухов из 10") 
-import time (1)
+time.sleep (1)
 print ("7 фаррухов из 10") 
 time.sleep (1)
 print ("6 фаррухов из 10") 
@@ -21,10 +21,9 @@ time.sleep (1)
 print ("2 фаррухов из 10") 
 time.sleep (5)
 print ("1 фаррухов из 10")
-import os 
 system ("cmatrix")
 time.sleep (15)
-import os 
-system ("exit") 
+system ("exit")
+
 
 
