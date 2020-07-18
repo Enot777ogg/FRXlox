926d7f37028f3af6712e348643f0f78abffc7b31
Enot777ogg <63520770+Enot777ogg@users.noreply.github.com>
2020-07-18 04:01:27 +0300

    Update frx.py

--- frx.py
+++ frx.py
@@ -8,28 +8,8 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init()
 print (Fore.RED + 'DDOS ATAKA!')
-print ("До самоуничтожения 10 фарухов из 10") 
-print ("9 фаррухов из 10") 
-time.sleep (1)
-print ("8 фаррухов из 10") 
-time.sleep (1)
-print ("7 фаррухов из 10") 
-time.sleep (1)
-print ("6 фаррухов из 10") 
-time.sleep (1)
-print ("5 фаррухов из 10") 
-time.sleep (1)
-print ("4 фаррухов из 10") 
-time.sleep (1)
-print ("3 фаррухов из 10") 
-time.sleep (1)
-print ("2 фаррухов из 10") 
-time.sleep (5)
-print ("1 фаррухов из 10")
-print(Back.BLUE + 'DANGER!')
-system.out ("cmatrix")
-time.sleep (15)
-system.out ("exit")
-
-
+a=[1,2,3,4,5,6,7,8,9,10]
+count+=1
+print (count,'Фарух из 10') 
+input() 
 
