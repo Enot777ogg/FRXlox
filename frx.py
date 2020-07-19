803096241338e31ecba89d7d17cf8d648947cfcc
Enot777ogg <63520770+Enot777ogg@users.noreply.github.com>
2020-07-19 20:42:08 +0300

    Update frx.py

--- frx.py
+++ frx.py
@@ -5,10 +5,10 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init()
 print (Fore.RED + 'Запуск Нейросети')
-time.sleep (5)
+time.sleep(5)
 print (Fore.RED + 'Нейросеть запущена')
 time_count = 10 
-for i in range(time_count, 0, -1): 
+   for i in range(time_count, 0, -1): 
 print("осталось {} секунд".format(i))
-time.sleep(1)
+  time.sleep(1)
 print("питон спиздили")
