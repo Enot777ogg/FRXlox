aacd5da29040016599cb81869278f6a8ce86385d
Enot777ogg <63520770+Enot777ogg@users.noreply.github.com>
2020-07-19 20:44:15 +0300

    Update frx.py

--- frx.py
+++ frx.py
@@ -8,7 +8,7 @@ print (Fore.RED + 'Запуск Нейросети')
 time.sleep(5)
 print (Fore.RED + 'Нейросеть запущена')
 time_count = 10 
-   for i in range(time_count, 0, -1): 
-print("осталось {} секунд".format(i))
+for i in range(time_count, 0, -1): 
+  print("осталось {} секунд".format(i))
   time.sleep(1)
 print("питон спиздили")
